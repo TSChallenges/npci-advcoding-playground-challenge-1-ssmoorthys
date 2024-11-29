ba7e98c7998bda2ca3c16a9a5229a536c3e5366e
ssmoorthys <30520393+ssmoorthys@users.noreply.github.com>
2024-11-29 23:43:56 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         // Test Case 1: Stock Analysis
-        StockPortfolio stock = new StockPortfolio("AAPL", 10, 150.0);
+        StockPortfolio stock = new StockPortfolio("BPCL", 10, 150.0);
+        System.out.println("Inital Price >>"+ stock.getCurrentPrice());
         stock.updatePrice(160.0);
+        System.out.println("Highest Price >>"+ stock.getHighestPrice());
         System.out.println(stock.calculateProfit());
         System.out.println(stock.getCurrentValue());
     }
